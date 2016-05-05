c37ed42144d2b5d57cd908e47c49df4be944a292
Adriaan de Vries <adriaan.de.vries@hotmail.com>
2016-05-05 18:37:04 +0200

    uses houghlines now

--- assignment4/e4_adriaan_devries_verna_dankers.m
+++ assignment4/e4_adriaan_devries_verna_dankers.m
@@ -2,6 +2,13 @@
 % Team: Adriaan de Vries (10795227), Verna Dankers (10761225)
 addpath('attachments');
 %%
-h = hough((rgb2gray(imread('shapes.png'))),[0.5 0.8],500,500);
+im = im2double(rgb2gray(imread('shapes.png')));
+h = hough(im,[0.5 0.8],500,500);
 h = h / (max(max(h))/4);
-imshow(h)
+%imshow(h)
+lines = houghlines(im, h, 1);
+imshow(im)
+hold on
+for n = 1:size(lines,1)
+    line(lines(n, 1:2), lines(n, 3:4))
+end
