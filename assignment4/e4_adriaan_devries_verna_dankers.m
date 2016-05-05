767fa3a5ac4031d3f630a733f11b1fe6ef7c25f7
Verna Dankers <verna_dankers@hotmail.com>
2016-05-05 13:09:27 +0200

    Added line for hough transformation

--- assignment4/e4_adriaan_devries_verna_dankers.m
+++ assignment4/e4_adriaan_devries_verna_dankers.m
@@ -1,8 +1,7 @@
 %% Beeldverwerken assignment 4, week 6
 % Team: Adriaan de Vries (10795227), Verna Dankers (10761225)
 addpath('attachments');
-run('vlfeat-0.9.20/toolbox/vl_setup');
-
-hough(
-
-
+%%
+h = hough((rgb2gray(imread('shapes.png'))),[0.5 0.8],500,500);
+h = h / (max(max(h))/4);
+imshow(h)
