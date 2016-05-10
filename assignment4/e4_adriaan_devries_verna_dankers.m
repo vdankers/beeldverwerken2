408a5f47476fe4e5004f948744390679c19b04a0
Verna Dankers <verna_dankers@hotmail.com>
2016-05-10 16:18:03 +0200

    Beautiful plots for dingen

--- assignment4/e4_adriaan_devries_verna_dankers.m
+++ assignment4/e4_adriaan_devries_verna_dankers.m
@@ -1,14 +1,15 @@
 %% Beeldverwerken assignment 4, week 6
 % Team: Adriaan de Vries (10795227), Verna Dankers (10761225)
 addpath('attachments');
+
+%% Plot hough transforms of several images
+plot_hough_transforms
+
 %%
-im = im2double(rgb2gray(imread('shapes.png')));
-h = hough(im,[0.5 0.8],500,500);
-h = h / (max(max(h))/4);
-%imshow(h)
-lines = houghlines(im, h, 1);
-imshow(im)
-hold on
-for n = 1:size(lines,1)
-    line(lines(n, 1:2), lines(n, 3:4))
-end
+
+% lines = houghlines(im, h, 1);
+% imshow(im)
+% hold on
+% for n = 1:size(lines,1)
+%     line(lines(n, 1:2), lines(n, 3:4))
+% end
