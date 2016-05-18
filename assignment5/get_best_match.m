614605c6e6f17424db4223cbc36f3af1026e3060
Adriaan de Vries <adriaan.de.vries@hotmail.com>
2016-05-18 17:42:56 +0200

    EVEN MORE COMMENTS, HOW DOES HE DO IT

--- assignment5/get_best_match.m
+++ assignment5/get_best_match.m
@@ -1,9 +1,12 @@
-%% get_best_match: ...
+%% get_best_match: returns the image from the database that correlates most
+%% with given image
 function best_match = get_best_match(imstruct, database)    
-  % get the best match
+  % instantiating variables
   best_match = zeros(size(database(1)));
   bestcorrelation = 0;
   
+  % go through database, every time a better match pops up than the best up
+  % to that point, it's the new best match.
   for i = 1: size(database,2)
     correlation = calc_correlation(imstruct.img , database(i).img);
     if correlation > bestcorrelation
